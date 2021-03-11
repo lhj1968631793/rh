07e65357acb434fc734a89a1047c7b27bb351e49
lx <18530217096@163.com>
2021-03-11 13:44:14 +0800

    代码自动生成

---
+++ src/main/java/com/code/bean/Table.java
@@ -0,0 +1,43 @@
+package com.code.bean;
+
+import com.code.util.StringUtils;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Table implements Serializable {
+
+    private String tableName;
+
+    private String comment;
+
+    private List<Cloumn> cloumns;
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public List<Cloumn> getCloumns() {
+        return cloumns;
+    }
+
+    public void setCloumns(List<Cloumn> cloumns) {
+        this.cloumns = cloumns;
+    }
+
+    public String getClassName(){
+      return  StringUtils.captureName(StringUtils.putOffUnderline(this.tableName));
+    }
+}
---
+++ src/main/java/com/code/constant/Constant.java
@@ -0,0 +1,19 @@
+package com.code.constant;
+
+public class Constant {
+   //修改生成的路径
+    public static String template_path = "D:\\ideajava\\code-parent\\src\\main\\resources\\qd";
+    public static String model_save_path = "D:\\ideajava\\KDS23\\qg-common-parent\\qg-common-module\\src\\main\\java\\com\\qg\\pojo";
+    public static String mapper_save_path = "D:\\ideajava\\KDS23\\qg-common-parent\\qg-common-dao\\src\\main\\resources\\mapper";
+    public static String class_mapper_save_path = "D:\\ideajava\\KDS23\\qg-common-parent\\qg-common-dao\\src\\main\\java\\com\\qg\\mapper";
+
+    public static String service_save_path = "D:\\ideajava\\KDS23\\qg-common-parent\\qg-common-service\\src\\main\\java\\com\\qg\\service";
+    public static String service_impl_save_path = "D:\\ideajava\\KDS23\\qg-common-parent\\qg-common-service\\src\\main\\java\\com\\qg\\service\\impl";
+
+    public static String controller_save_path = "D:\\ideajava\\KDS23\\qg-common-parent\\qg-common-controller\\src\\main\\java\\com\\qg\\controller";
+    public static String jsp_save_path = "D:\\ideajava\\KDS23\\qg-common-parent\\qg-common-control\\src\\main\\webapp";
+
+
+
+    public static String sql_save_path = "D:\\ideajava\\KDS23";
+}
